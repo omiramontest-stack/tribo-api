217bf655d6363e2559bacd12e87bdcc5a4d5c172
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2026-05-23 19:20:13 -0600

    fix: add header

--- src/http/plugins/cors.ts
+++ src/http/plugins/cors.ts
@@ -11,6 +11,7 @@ export default fp(async (app: FastifyInstance) => {
     origin: origins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-organization-id', 'ngrok-skip-browser-warning'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-organization-id', 'x-request-id', 'x-trace-id', 'ngrok-skip-browser-warning'],
+    exposedHeaders: ['x-request-id', 'x-trace-id'],
   })
 })
